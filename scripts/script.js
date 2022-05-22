342fbbc62de26644d3234d9498c8edd7264d4dcb
sampleauto <samuel.r.pilato@gmail.com>
2022-05-22 18:15:48 -0500

    Edited script

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,6 @@ if(hours > 12) {
   hours = hours - 12;
   ampm = "pm";
 }
-var minutes = today.getMinutes();
-minutes = String(minutes).padStart(2,'0');
+var minutes = String(today.getMinutes()).padStart(2,'0');
 var time = hours + ":" + minutes + ampm;
 document.write(time);
