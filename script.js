cfa25c49a36306e596cd05ef59a9ca9939c165ce
sampleauto <samuel.r.pilato@gmail.com>
2022-05-22 16:25:57 -0500

    Edited format of time on page.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const zeroPad = (num, places) => String(num).padStart(places, '0')
 var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var hours = today.getHours();
+var ampm = "am";
+if(hours > 12) {
+  hours = hours - 12;
+  ampm = "pm";
+}
+var minutes = zeroPad(today.getMinutes(),2);
+var time = hours + ":" + minutes + ampm;
 document.write(time);
